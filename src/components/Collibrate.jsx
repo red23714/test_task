a76a4729d13fc1f501cfd7d06fde457361a4ead5
red <redredovich0@gmail.com>
2023-06-19 11:26:42 +0300

    CSS improves

--- src/components/Collibrate.jsx
+++ src/components/Collibrate.jsx
@@ -5,40 +5,51 @@ const styles = createUseStyles({
     myCollibrate:
     {
         display: (props) => props.saved ? 'none' : 'flex',
-        justifyContent: 'flex-end',
-        flexDirection: 'column',
-        alignItems: 'flex-end',
         padding: 10,
-        margin: 'auto' 
+        flexDirection: 'column',
+        '& $textarea':
+        {
+            flex: [1, 1, 'auto'],
+            background: (props) => props.valid ? 'white' : '#D53032',
+            resize: 'none',
+            border: [2, '#000', 'solid'],
+            padding: 10,
+            fontFamily: 'Kaushan Script',
+            paddingBottom: 20,
+        },
+        '& $p':
+        {
+            flexGrow: 0,
+        }
     },
 
-    myTextarea:
+    myButtons:
     {
-        background: (props) => props.valid ? 'white' : '#D53032',
-        width: '97vw',
-        height: '80vh',
-        resize: 'none',
-        border: [2, '#000', 'solid'],
-        padding: 10,
-        fontFamily: 'Kaushan Script',
-        paddingBottom: 20
+        flexGrow: 0,
+        marginLeft: '65%',
+    },
+
+    myButtonSave:
+    {
+        cursor: 'pointer',
+        backgroundColor: (props) => props.valid ? 'yellow' : 'white',
+        transition: ['background', 'color'],
+        transitionDuration: 150,
+        '&:hover':
+        {
+            backgroundColor: '#4CAF50',
+            color: 'white'
+        },
+        '&:active':
+        {
+            backgroundColor: 'red',
+            color: 'white'
+        }
     },
 
     mySaved:
     {
         display: (props) => props.saved ? '' : 'none'
-    },
-
-    myButtonSave:
-    {
-        alignSelf: 'center',
-        cursor: 'pointer',
-        backgroundColor: (props) => props.valid ? 'yellow' : 'white',
-        '&:hover':
-        {
-            backgroundColor: '#4CAF50',
-            color: 'white'
-        }
     }
 });
 
@@ -49,14 +60,13 @@ const Collibrate = () => {
     const bracket_coll = (text_in) => {
         let brackets = "()[]{}<>";
         const stack = [];
-        
-        if(text_in)
-        {
+
+        if (text_in) {
             for (let char of text_in) {
                 let is_bracket = brackets.indexOf(char);
                 if (is_bracket >= 0) {
                     if (is_bracket % 2 !== 0) {
-                        if (stack.lenght === 0) return false; 
+                        if (stack.lenght === 0) return false;
 
                         let last_element = stack.pop();
                         if (last_element !== is_bracket - 1) {
@@ -72,53 +82,51 @@ const Collibrate = () => {
             if (stack.length === 0) return true;
             else return false;
         }
-        else
-        {
+        else {
             return true;
         }
     }
 
     const isValid = useMemo(() => bracket_coll(text), [text]);
 
-    const classes = styles({valid: isValid, saved: isSaved});
+    const classes = styles({ valid: isValid, saved: isSaved });
 
-    const handleSave = () =>
-    {
+    const handleSave = () => {
         localStorage.setItem('text', text);
 
         setIsSaved(true);
     }
 
-    const handleClose = () =>
-    {
+    const handleClose = () => {
         setText(localStorage.getItem('text'));
 
         setIsSaved(false);
     }
 
-    const handleErase = () =>
-    {
+    const handleErase = () => {
         setText('');
     }
 
-    useEffect(() => {localStorage.getItem('text') ? setText(localStorage.getItem('text')) : setText('')}, []);
+    useEffect(() => { localStorage.getItem('text') ? setText(localStorage.getItem('text')) : setText('') }, []);
 
     return (
         <div>
             {isSaved ? (
-            <div className={classes.mySaved}>
-                <p>Текст успешно сохранен</p>
-                <button onClick={handleClose}>Закрыть</button>
-            </div>
+                <div className={classes.mySaved}>
+                    <p>Текст успешно сохранен</p>
+                    <button onClick={handleClose}>Закрыть</button>
+                </div>
             ) : (
-            <div className={classes.myCollibrate}>
-                <textarea className={classes.myTextarea} onChange={(e) => setText(e.target.value)}
-                    value={text}
-                    placeholder='Type text' />
-                <p> {isValid.toString()} </p>
-                <button className={classes.myButtonSave} onClick={handleSave} disabled={!isValid}>Сохранить</button>
-                <button onClick={handleErase}>Очистить</button>
-            </div>
+                <div className={classes.myCollibrate}>
+                    <textarea onChange={(e) => setText(e.target.value)}
+                        value={text}
+                        placeholder='Type text' />
+                    <p> {isValid.toString()} </p>
+                    <div className={classes.myButtons}>
+                        <button className={classes.myButtonSave} onClick={handleSave} disabled={!isValid}>Сохранить</button>
+                        <button onClick={handleErase}>Очистить</button>
+                    </div>
+                </div>
             )}
         </div>
     );
